edc0cab6ef2418f5a04f0d2279162c065175c0df
KomisarzRyba <antek.olesik@gmail.com>
2023-07-12 22:20:59 +0200

    Adjust postcss config

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 export const Navbar = async () => {
 	const session = await getAuthSession();
 	return (
-		<nav className='fixed top-0 inset-x-0 h-fit py-4 z-10'>
+		<nav className='fixed top-0 inset-x-0 h-fit py-2 z-10 bg-background'>
 			<div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-6'>
 				<Link href='/' className='flex gap-2 items-center'>
 					<Icons.logo className='w-8 h-8 sm:w-6 sm:h-6' />
